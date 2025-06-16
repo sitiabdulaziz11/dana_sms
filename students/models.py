19b5e28bdfac47f6c51896f85bb6556d2032f4c8
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-06-16 10:03:18 +0300

    student model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    """Studentes information.
+    """
+    first_name = models.CharField(max_length=100)
+    middlename = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=15)
+    email = models.EmailField(max_length=254)
+    password = 
+    birth_date = models.DateField(null=True, blank=True)
+    age = models.IntegerField()
+    image_file = models.ImageField()
+    address = models.CharField(max_length=60)
+    registration_date = models.DateField(auto_now_add=True)
+
+    # registration date =, phone_no from parent or? grade, section other separet table or?, email , password good or?
+
