2b0b4a080efa51c390ef24b7f6e8d2836736c736
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-07-02 10:48:06 +0300

    dana sms db

--- payments/models.py
+++ payments/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from students.models import StudentRegistration
+
 # Create your models here.
 
 MONTH_CHOICES = [
@@ -29,17 +31,17 @@ PAYMENT_TYPE_CHOICES = [
 class Payment(models.Model):
     """Model which handles payment status.
     """
-    payer_name = models.CharField(max_length=120)
+    payer_name = models.CharField(max_length=120)   #?
     amount = models.FloatField()
     debited_date = models.DateField(auto_now_add=True)
     debited_month = models.CharField(max_length=120, choices=MONTH_CHOICES)
     payment_type= models.CharField(max_length=120, choices=PAYMENT_TYPE_CHOICES )  # monthly or for after class redding
-    payment_status = models.CharField(max_length=10,
+    payment_status = models.CharField(max_length=20,
         choices=PAYMENT_STATUS_CHOICES,
         default='pending')  # payid or not payid
  
-    # Relation with student model
-    student = models.ForeignKey('StudentRegistration', on_delete=models.CASCADE, related_name='payments')
+    # Relation with student model 
+    student = models.ForeignKey(StudentRegistration, on_delete=models.CASCADE, related_name='payments') #  payer name need or not student is enough or?
 
     def __str__(self):
-        return f"{self.payer_name} - {self.student} - {self.debited_month} - {self.payment_type} - {self.payment_status}, {self.debited_date}"
+        return f"{self.payer_name} - {self.student} - {self.debited_month} - {self.payment_type} - {self.payment_status} - {self.debited_date}"
