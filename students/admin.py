6717298c6c1d48e59f302475df5134ac3f2563c3
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-07-21 13:56:25 +0300

    sms

--- students/admin.py
+++ students/admin.py
@@ -5,6 +5,30 @@ from payments.models import Payment
 
 # Register your models here.
 
+    
+class PaymentMonthFilter(admin.SimpleListFilter):
+    """ Filter by month (from related payment model)
+    """
+    title = 'Monthly payment'
+    parameter_name = 'month'
+
+    def lookups(self, request, model_admin):
+        """Define dropdown filter options in admin
+        """
+        months = Payment.objects.values_list('debited_month', flat=True).distinct()  # To get distinct months from Payment model.
+        return [(month, month) for month in months if month]
+        # return Payment.MONTH_CHOICES
+    
+    def queryset(self, request, queryset):
+        """Actually filter records based on selected value.
+        """
+        if self.value():
+            paid_students_ids = Payment.objects.filter(debited_month=self.value(), payment_status='').values_list('student_id', flat=True)
+            if self.value() == 'paid':
+                return queryset.filter(id__in=paid_students_ids)
+            return queryset.exclude(id__in=paid_students_ids)
+        return queryset
+    
 class PaymentStatusFilter(admin.SimpleListFilter):
     """
     """
@@ -20,39 +44,15 @@ class PaymentStatusFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset):
         """
         """
+        month = request.GET.get('month')
+        if not month:
+            return queryset
+        
+        paid_students_ids = Payment.objects.filter(debited_month=month, payment_status='paid').values_list('student_id', flat=True)
         if self.value() == 'paid':
-            # Get student IDs that have at least one paid payment
-            # paid_ids = Payment.objects.filter(payments__payment_status='paid').values_list('student_id', flat=True)
-            # return queryset.filter(id__in=paid_ids)
-            return queryset.filter(payments__payment_status='paid')
+            return queryset.filter(id__in=paid_students_ids)
         elif self.value() == 'pending':
-            # pending_ids = Payment.objects.filter(payments__payment_status='pending').values_list('student_id', flat=True)
-            # return queryset.filter(id__in=pending_ids)
-            return queryset.exclude(payments__payment_status='paid')
-        return queryset
-    
-class PaymentMonthFilter(admin.SimpleListFilter):
-    """ Filter by month (from related payment model)
-    """
-    title = 'Monthly payment'
-    parameter_name = 'debited_month'
-
-    def lookups(self, request, model_admin):
-        """Define dropdown filter options in admin
-        """
-        months = Payment.objects.values_list('debited_month', flat=True).distinct()  # To get distinct months from Payment model.
-        return [(month, month) for month in months if month]
-        # return Payment.MONTH_CHOICES
-    
-    def queryset(self, request, queryset):
-        """Actually filter records based on selected value.
-        """
-        if self.value():
-            paid_students_ids = Payment.objects.filter(debited_month=self.value(), payment_status='paid').values_list('student_id', flat=True)
-            if self.value() == 'paid':
-                return queryset.filter(id__in=paid_students_ids)
             return queryset.exclude(id__in=paid_students_ids)
-            # return queryset.filter(payments__debited_month=self.value())
         return queryset
 
 
